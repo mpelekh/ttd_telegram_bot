e24609c519aea721f25d638fe152f9e54c6bfacc
nick.peleh <nick.peleh@gmail.com>
2016-10-11 22:34:22 +0300

    Add telegram bot

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,28 @@
 const
     express = require('express'),
-    router = express.Router();
-
-router
-    .post('/send', (req, res, next) => {
-        console.log(req.body);
-        res.json({success: true});
-    });
+    router  = express.Router();
 
 module.exports = router;
+
+const
+    TelegramBot = require('node-telegram-bot-api'),
+    botToken    = require('../../config/bot.json').token,
+    recipients  = require('../../config/recipients.json'),
+    bot         = new TelegramBot(botToken, {polling: true});
+
+bot.on('message', botOnMessage);
+
+router
+    .post('/send', (req, res) => {
+        recipients.forEach(recipient => bot.sendMessage(recipient.chatId, JSON.stringify(req.body)));
+
+        res
+            .status(200)
+            .json({success: true});
+    });
+
+
+function botOnMessage(msg) {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Hello!');
+}
